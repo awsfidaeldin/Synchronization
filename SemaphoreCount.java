72f208905538f990f503c35884c5372ee0ee7cc5
aws.yousif@gmail.com <altaie@qu.edu.qa>
2020-06-20 20:00:50 +0300

    Second Attempt for SemaphoreCount.java

--- SemaphoreCount.java
+++ SemaphoreCount.java
@@ -1,8 +1,10 @@
 import java.util.concurrent.*;
 
+//This part will be xecuted by multiple Threads.
 class Task implements Runnable
 {
 	Semaphore semaphore=new Semaphore(2);
+	//Need to implement Run Method.
 	public void run() {
 		try
 		{
@@ -10,6 +12,7 @@ class Task implements Runnable
 			System.out.println("Being run by " + Thread.currentThread().getName());
 			for(int i=0;i<5;i++)
 				System.out.println("Thread Running ... " + Thread.currentThread().getName());
+				//Delay of 2 seconds for each Iteration.
 				Thread.sleep(2000);
 		}
 		catch(Exception e)
@@ -29,15 +32,19 @@ class Task implements Runnable
 public class SemaphoreCount {
 
 	public static void main(String[] args) {
+		//Create an Object of Task Class.
 		Task task=new Task();
+		//Supply the Task so the constructor of the Thread will execute the same Task.
 		Thread t1=new Thread(task);
 		Thread t2=new Thread(task);
 		Thread t3=new Thread(task);
 		Thread t4=new Thread(task);
+		// Name the Threads
 		t1.setName("Thread-1");
 		t2.setName("Thread-2");
 		t3.setName("Thread-3");
 		t4.setName("Thread-4");
+		// Start the Threads
 		t1.start();
 		t2.start();
 		t3.start();
